9dfe0a36a59871663b1a26421e325394c3e7555a
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2026-05-09 11:34:10 -0400

    refactor(db): Reorganiza as tabelas conforme a API

--- database/schema.sql
+++ database/schema.sql
@@ -1,25 +1,49 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password TEXT NOT NULL,
     token TEXT
 );
 
-CREATE TABLE IF NOT EXISTS status (
+CREATE TABLE document_status (
     id SMALLINT PRIMARY KEY,
     description VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS documents (
+CREATE TABLE documents (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    api_id VARCHAR(255) NOT NULL,
+    certisign_document_id TEXT NOT NULL,
     file_name TEXT NOT NULL,
-    status SMALLINT NOT NULL,
+    sign_url TEXT,
+    status_id SMALLINT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (status) REFERENCES status(id)
+    FOREIGN KEY (status_id) REFERENCES document_status(id)
 );
 
+CREATE TABLE signature_types (
+    id SMALLINT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE signatures (
+    id SERIAL PRIMARY KEY,
+    document_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    type_signature_id SMALLINT NOT NULL,
+    status_id SMALLINT,
+    step SMALLINT DEFAULT 1,
+    certisign_signer_uid TEXT,
+    signed_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (document_id) REFERENCES documents(id),
+    FOREIGN KEY (type_signature_id) REFERENCES signature_types(id),
+    FOREIGN KEY (status_id) REFERENCES document_status(id)
+);
--- database/seed.sql
+++ database/seed.sql
@@ -1,5 +1,15 @@
-INSERT INTO status (id, description)
+INSERT INTO document_status (id, description)
 VALUES
-    (1, 'Pendente'),
-    (2, 'Assinado'),
-    (3, 'Recusado');
+    (1, 'Criado'),
+    (2, 'Pendente de Assinatura'),
+    (3, 'Parcialmente Assinado'),
+    (4, 'Assinado'),
+    (5, 'Recusado'),
+    (6, 'Cancelado'),
+    (7, 'Expirado'),
+    (8, 'Erro');
+
+INSERT INTO signature_types (id, description)
+VALUES
+    (1, 'Certificado Digital'),
+    (2, 'Assinatura Eletronica');
