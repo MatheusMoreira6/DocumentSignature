df679f782438c924d2dc5f4ca27389d0ba6b8d62
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2026-05-10 17:19:24 -0400

    revert: Corrige status de assinatura

--- database/seed.sql
+++ database/seed.sql
@@ -1,13 +1,11 @@
 INSERT INTO document_status (id, description)
 VALUES
-    (1, 'Criado'),
-    (2, 'Pendente de Assinatura'),
-    (3, 'Parcialmente Assinado'),
-    (4, 'Assinado'),
-    (5, 'Recusado'),
-    (6, 'Cancelado'),
-    (7, 'Expirado'),
-    (8, 'Erro');
+    (1, 'Assinatura Pendente'),
+    (2, 'Parcialmente Assinado'),
+    (3, 'Assinado'),
+    (4, 'Recusado'),
+    (5, 'Cancelado'),
+    (6, 'Expirado');
 
 INSERT INTO signature_types (id, description)
 VALUES
